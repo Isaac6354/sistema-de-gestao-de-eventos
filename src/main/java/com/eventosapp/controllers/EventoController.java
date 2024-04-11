1fda88b312eec96f9358c61dcd72f074393a9728
Isaac <isaacemanuel2@gmail.com>
2024-04-10 22:51:21 -0300

    feat: adicionada a rota no conroller e a página na view para os detalhes do evento

--- src/main/java/com/eventosapp/controllers/EventoController.java
+++ src/main/java/com/eventosapp/controllers/EventoController.java
@@ -2,6 +2,7 @@ package com.eventosapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -50,6 +51,14 @@ public class EventoController {
 		return mv;
 	}
 	
+	//Aqui ele vai retornar o código de cada evento.
+	//Assim que o usuário clicar no evento ele será direcionado para os detalhes daquele evento
+	@RequestMapping("/{codigo}")
+	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo){
+		Evento evento = er.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		mv.addObject("evento", evento);
+		return mv;
+	}
 	
-
 }
--- src/main/java/com/eventosapp/repository/EventoRepository.java
+++ src/main/java/com/eventosapp/repository/EventoRepository.java
@@ -6,5 +6,5 @@ import com.eventosapp.models.Evento;
 
 //Ao extender da classe CrudRepository(), conseguiremos utilizar métodos como salvar, editar e deletar da classe
 public interface EventoRepository extends CrudRepository<Evento, String>{
-
+	Evento findByCodigo(long codigo);
 }
